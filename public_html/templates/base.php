273930e61909fe6e2d74bd8d3ffdfa83528989c1
ValeJappo <cioffval@gmail.com>
2022-10-31 08:39:13 +0100

    fix

--- public_html/templates/base.php
+++ public_html/templates/base.php
@@ -1,5 +1,5 @@
 <?php
-require_once $_SERVER['DOCUMENT_ROOT'].'/vendor/autoload.php';
+require_once $_SERVER['DOCUMENT_ROOT'].'/../vendor/autoload.php';
 require_once $_SERVER['DOCUMENT_ROOT'].'/identity/functions/auth.php';
 require_once $_SERVER['DOCUMENT_ROOT'].'/templates/auth.php';
 
