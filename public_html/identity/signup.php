1aa704fae8d3d641f9819b95d05a2a0a6a4660b0
ValeJappo <cioffval@gmail.com>
2022-10-31 08:41:12 +0100

    fix

--- public_html/identity/signup.php
+++ public_html/identity/signup.php
@@ -1,6 +1,6 @@
 <?php
 require_once $_SERVER['DOCUMENT_ROOT'].'/../vendor/autoload.php';
-require $_SERVER['DOCUMENT_ROOT'].'/templates/base.php';
+require_once $_SERVER['DOCUMENT_ROOT'].'/templates/base.php';
 require_once $_SERVER['DOCUMENT_ROOT'].'/templates/auth.php';
 
 use Kreait\Firebase\Factory;
